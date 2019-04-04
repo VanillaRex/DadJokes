d981aa88f33855f7d35147ebfd514cbce8af230a
lxst <lxst@gmail.com>
2019-04-04 10:42:46 +1000

    Added joke

--- DadJokes/JokeStore.swift
+++ DadJokes/JokeStore.swift
@@ -31,6 +31,7 @@ struct JokeStore {
   /// we NEED more jokes here!!!
   private var storage: [Joke] = [
     Joke(description: "Why don't programmers like being outside?", punchline: "Because there's too many bugs"),
+    Joke(description: "Hello", punchline: "World"),
     Joke(description: "Why don't bachelors like Git?", punchline: "Because they are afraid to commit."),
     Joke(description: "What does the father play with his son?", punchline: "Try Catch"),
   ]
